d1184e73bce31bdf12213318e06eb7e075025ac0
toufiq-austcse <toufiq-austcse@gmail.com>
2020-01-03 00:50:57 +0600

    Chnages added

---
+++ lib/routes/crmRoutes.ts
@@ -0,0 +1,36 @@
+import {NextFunction, Request, Response} from 'express'
+import {ContactController} from "../controllers/crmController";
+
+export class Routes {
+    public contactController: ContactController = new ContactController();
+
+    public routes(app): void {
+        app.route('/')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'GET request successfulll!!!!'
+                })
+            });
+
+        // Contact
+        app.route('/contact')
+        // GET endpoint
+            .get((req: Request, res: Response, next: NextFunction) => {
+                //Middleware
+                if (req.params.publickey != 'sady') {
+                    res.status(401).send('You shall not pass!');
+                } else {
+                    next();
+                }
+            }, this.contactController.getContacts)
+            // POST endpoint
+            .post(this.contactController.addNewContact);
+
+        // Contact detail
+        app.route('/contact/:contactId')
+        // get specific contact
+            .get(this.contactController.getContactWithID)
+            .put(this.contactController.updateContact)
+            .delete(this.contactController.deleteContact)
+    }
+}
