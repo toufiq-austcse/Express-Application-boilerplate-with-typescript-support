3caf898dc60db80cb072589041f67c64fa0df471
toufiq-austcse <toufiq-austcse@gmail.com>
2020-05-31 12:16:48 +0600

    Changes in package.json

--- dist/types/type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TYPES = {
-    UserModel: 'UserModel',
-    LinkModel: 'LinkModel',
-    AuthMiddleware: 'AuthMiddleware'
-};
-exports.TYPES = TYPES;
-//# sourceMappingURL=type.js.map
